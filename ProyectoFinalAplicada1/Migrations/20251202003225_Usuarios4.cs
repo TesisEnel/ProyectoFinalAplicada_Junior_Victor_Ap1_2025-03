b97f3c23e0400ba47d66f23c5a99d5f2bd694b04
Ramon Junior Urena Vasquez <ramon_urena2@ucne.edu.do>
2025-12-05 09:30:08 -0400

    despues de chekeo

---
+++ ProyectoFinalAplicada1/Migrations/20251202003225_Usuarios4.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoFinalAplicada1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Usuarios4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
